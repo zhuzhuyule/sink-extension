d6937ae35205db3ed11ef157ee8ddd9747146d03
zhuzhuxia <zhuzhuyule@126.com>
2024-08-21 01:34:18 +0800

    style: update the list scroll position

--- src/options/components/Links.tsx
+++ src/options/components/Links.tsx
@@ -9,14 +9,14 @@ import ComputerSvg from '@src/assets/computer.svg?react';
 export const Links = () => {
   const links = useAtomValue(linksAtom);
   return (
-    <div className='m-5 flex flex-1 flex-col overflow-hidden'>
+    <div className='m-5 flex flex-1 flex-col'>
       <h2 className='flex items-center justify-start gap-4 text-center text-2xl font-bold'>
         <Logo />
         Links
         {links && <ComputerSvg className='ml-auto w-10 h-10 cursor-pointer text-green-500' />}
       </h2>
       <SplitLine />
-      <div className='grid grid-cols-[repeat(auto-fit,minmax(300px,1fr))] gap-4 overflow-y-auto'>
+      <div className='grid grid-cols-[repeat(auto-fit,minmax(300px,1fr))] gap-4'>
         {links
           ? links.map(link => <LinkTag url={link.url} shortKey={link.slug} />)
           : ''}
--- src/options/components/Options.tsx
+++ src/options/components/Options.tsx
@@ -8,7 +8,7 @@ const Options = () => {
   const isShow = useAtomValue(settingModalAtom);
 
   return (
-    <div className='flex h-screen w-screen bg-gray-100 p-8'>
+    <div className='flex h-screen w-screen bg-gray-100 p-8 overflow-y-auto'>
       <div
         className={`fixed left-[50vw] top-[50vh] -translate-x-1/2 -translate-y-1/2 transform transition-all duration-300 ease-in-out ${
           isShow ? 'scale-0 opacity-0' : 'scale-100 opacity-100'
